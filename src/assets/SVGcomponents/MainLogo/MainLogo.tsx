38b6820d2891e3f64053f8c18ad5c9de52b8a010
ArsalanTauseef <232355832+ArslanTauseef@users.noreply.github.com>
2025-12-30 19:04:57 +0500

    #5

--- src/assets/SVGcomponents/MainLogo/MainLogo.tsx
+++ src/assets/SVGcomponents/MainLogo/MainLogo.tsx
@@ -1,43 +1,85 @@
 import * as React from "react";
-const MainLogo = (props: React.SVGProps<SVGSVGElement>) => (
+const SVGComponent = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    id="MainLogo"
-    viewBox="0 0 357 365"
+    className="SVGShards"
+    viewBox="0 0 1551 1390"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <g id="MainLogoGroup">
-      <path
-        id="midstrip"
-        d="M105.691 206.169H268V243.169H79L105.691 206.169Z"
-        fill="white"
-      />
-      <path
-        id="shortstrip"
-        d="M137.667 154.169H233V191.169H112L137.667 154.169Z"
-        fill="white"
-      />
-      <path
-        id="Logn strip"
-        d="M155.805 129.758L160.883 59.7824L337 278.454L274.86 278.454L155.805 129.758Z"
-        fill="url(#paint0_linear_30_18)"
-      />
+    <g id="Shards">
+      <g id="FourShards">
+        <path
+          id="Rectangle4"
+          d="m774.775 741.759 292.345-97.117 197.93-63.464 146.14 64.459 139.2 67.581-206.86 95.625-309.45 5.402z"
+          fill="url(#paint0_linear_44_27)"
+        />
+        <path
+          id="Rectangle3"
+          d="m708.852 702.833 62.223 246.721 40.245 166.896-61.912 116.49-64.061 110.58-42.477-186.05-9.393-245.411z"
+          fill="url(#paint1_linear_44_27)"
+        />
+        <path
+          id="Rectangle2"
+          d="m748.668 646.114-192.402 45.805-130.129 29.549-89.839-49.369-85.221-50.978 145.037-31.085 190.805-4.733z"
+          fill="url(#paint2_linear_44_27)"
+        />
+        <path
+          id="Rectangle1"
+          d="m802.727 659.58-48.73-190.856-31.527-129.106 47.995-90.199 49.674-85.627 33.29 143.925 7.638 189.894z"
+          fill="url(#paint3_linear_44_27)"
+        />
+      </g>
     </g>
     <defs>
       <linearGradient
-        id="paint0_linear_30_18"
-        x1={251.005}
-        y1={162.886}
-        x2={185.706}
-        y2={218.278}
+        id="paint0_linear_44_27"
+        x1={662.061}
+        y1={840.492}
+        x2={1594.06}
+        y2={494.492}
         gradientUnits="userSpaceOnUse"
       >
-        <stop stopColor="#5CE3EA" />
-        <stop offset={0.5} stopColor="#458ABE" />
-        <stop offset={1} stopColor="#2E3192" />
+        <stop offset={0.303} stopColor="#5ce3ea" />
+        <stop offset={0.74} stopColor="#458abe" />
+        <stop offset={1} stopColor="#2e3192" />
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear_44_27"
+        x1={700.5}
+        y1={892.999}
+        x2={682.561}
+        y2={1442.99}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5ce3ea" />
+        <stop offset={0.779} stopColor="#458abe" />
+        <stop offset={1} stopColor="#2e3192" />
+      </linearGradient>
+      <linearGradient
+        id="paint2_linear_44_27"
+        x1={752.145}
+        y1={619.492}
+        x2={18.523}
+        y2={653.28}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0.263} stopColor="#5ce3ea" />
+        <stop offset={0.646} stopColor="#458abe" />
+        <stop offset={0.928} stopColor="#2e3192" />
+      </linearGradient>
+      <linearGradient
+        id="paint3_linear_44_27"
+        x1={792}
+        y1={540.499}
+        x2={825.501}
+        y2={-19.501}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5ce3ea" />
+        <stop offset={1} stopColor="#2e3192" />
       </linearGradient>
     </defs>
   </svg>
 );
-export default MainLogo;
+export default SVGComponent;
--- src/components/layout/header/tsx/Header.tsx
+++ src/components/layout/header/tsx/Header.tsx
@@ -1,27 +1,29 @@
-import "../styles/header.css";
-import "../styles/header__right.css"
-import "../styles/header__left.css"
+import "../styles/header/header.css";
+import "../styles/header_left/css/header__left.css";
+import "../styles/header_right/css/header__right.css";
+import "../styles/header_right/animations/ar-cta/ar-cta.css";
 
 import MainLogo from "../../../../assets/SVGcomponents/MainLogo/MainLogo";
+import ArslanTauseef from "../../../../assets/SVGcomponents/ArslanTauseef/ArslanTauseef";
 
 export const Header = () => {
   return (
-    <>
-      <header className="ar-header">
-        <ul id="ar-ul">
-            <li><MainLogo /></li>
-            <li>
-                <nav className="ar-nav">
-                    <ul>
-                        <li><button className="h-buttons">HOME</button></li>
-                        <li><button className="h-buttons">PROJECTS</button></li>
-                        <li><button className="h-buttons">ABOUT ME</button></li>
-                        <li><button className="h-buttons">HIRE ME</button></li>
-                    </ul>
-                </nav>
-            </li>
+    <header className="ar-header">
+      <div className="ar-header-left">
+        <MainLogo />
+        <ArslanTauseef />
+      </div>
+      {/* SEPERATOR */}
+      <nav className="ar-header-right" aria-label="Main navigation">
+        <ul className="ar-nav-list">
+          <li><a href="/">Home</a></li>
+          <li><a href="/projects">Projects</a></li>
+          <li><a href="/about">About Me</a></li>
         </ul>
-      </header>
-    </>
+        <div className="ar-cta">
+          <a href="/contact">Hire Me</a>
+        </div>
+      </nav>
+    </header>
   );
 };
