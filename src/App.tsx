c3a31b4e947077f665c13d9dfc83c4df780e9943
ArsalanTauseef <232355832+ArslanTauseef@users.noreply.github.com>
2026-01-08 19:31:25 +0500

    #14

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Header } from "./components/layout/header/Header";
 import { Middle } from "./components/layout/middle/Middle";
 import { Footer } from "./components/layout/footer/Footer";
+
 function App() {
   return (
     <>
